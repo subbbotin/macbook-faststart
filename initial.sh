5232f5922fbe5b5ae1eb436f36ccd50e2b36d507
Sergey <41997588+sergeyartemenko@users.noreply.github.com>
2018-08-23 12:20:16 +0300

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -10,18 +10,22 @@ brew update
 # Upgrade any already installed formulae
 brew upgrade --all
 # Install desired cask packages
-brew cask install teamviewer
 brew cask install skype
 brew cask install slack
 brew cask install the-unarchiver
-brew cask install zoomus
 brew cask install atom
 brew cask install vlc
 brew cask install telegram
 brew cask install ccleaner
+brew cask install teamviewer-quicksupport
 # Remove brew cruft
 brew cleanup
 # Remove cask cruft
-brew cask cleanup
+#brew cask cleanup
+#Calling `brew cask cleanup` is deprecated and will be disabled on 2018-09-30! Use `brew cleanup` instead.
 # Download Google Drive File Stream
 open https://dl.google.com/drive-file-stream/googledrivefilestream.dmg
+#google drive stream require password prompt
+# Download Zoom
+open https://zoom.us/client/latest/Zoom.pkg
+#all pkg require password prompt
