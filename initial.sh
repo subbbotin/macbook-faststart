274574afde5c526337360ed6be1204533713d383
Artem Subbotin <subbbotin@frtk.ru>
2018-09-18 16:54:06 +0300

    fixes
    remove telegram, add MS Office

--- initial.sh
+++ initial.sh
@@ -15,16 +15,14 @@ brew cask install slack
 brew cask install the-unarchiver
 brew cask install atom
 brew cask install vlc
-brew cask install telegram
 brew cask install teamviewer-quicksupport
-# Remove brew cruft
 brew cleanup
-# Remove cask cruft
+# download packages is better than cask
 open https://dl.google.com/drive-file-stream/googledrivefilestream.dmg
-# google drive stream require password prompt
 open https://zoom.us/client/latest/Zoom.pkg
-# all pkg require password prompt
+# drivers for printers
 open https://o.cses.konicaminolta.com/file/Default.aspx?FilePath=DL/201606/10065530/BHC3100PPSMacOS109_150MU.dmg
 open https://o.cses.konicaminolta.com/file/Default.aspx?FilePath=DL/201606/27031742/BHC554ePSMacOS109_540MU.dmg
 open https://ftp.hp.com/pub/softlib/software12/HP_Quick_Start/osx/Applications/HP_Easy_Start.app.zip
-# drivers for printers
+# MS Office
+open https://drive.google.com/open?id=1nsNBvFpnBiezkEf28Ke3nHI0-ntjDWqO
